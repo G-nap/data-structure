1b237d28c6651cab71c67406beb4062244b79423
bebpandy <bebpandy@gmail.com>
2020-09-02 16:47:08 +0700

    add

--- Lab/week 3_Stack/2 brokenPlate.py
+++ Lab/week 3_Stack/2 brokenPlate.py
@@ -1,1 +1,29 @@
-n = input("Enter Input : ")
+class Stack:
+    def __init__(self):
+         self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+n = input('Enter Input : ').split(',')  
+s = Stack()
+for e in n:
+    w,f = e.split()
+    if s.size() and s.peek()[0] < int(w):
+        print(s.pop()[1])
+        while (s.size() and s.peek()[0] < int(w)):
+            print(s.pop()[1])
+    s.push((int(w),int(f)))
+     
